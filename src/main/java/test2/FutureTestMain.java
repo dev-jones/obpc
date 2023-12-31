84b73bcb2c3ccabefb0d2760605cea1923266de6
dev-jones <dev-jones@github.com>
2023-12-31 14:36:59 +0900

    Future 예제 추가.. 속도가 개선되진않음

--- src/main/java/test2/FutureTestMain.java
+++ src/main/java/test2/FutureTestMain.java
@@ -5,49 +5,35 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import com.devjones.obpc.domain.ParserThread;
 import com.devjones.obpc.domain.Product;
 
 public class FutureTestMain {
 
-	public static void main(String[] args) {
-		ExecutorService executorService = Executors.newFixedThreadPool(
-                Runtime.getRuntime().availableProcessors()
-        );
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		String[] query = { "갤럭시 GALAX 지포스 RTX 4060 Ti OC D6 8GB", "AMD 라이젠5-5세대 7500F (라파엘) (멀티팩(정품))",
+				"ASRock B650M-HDV/M.2 대원씨티에스", "삼성전자 DDR5-5600 (16GB) PC5-44800" };
 
-        System.out.println("[작업 처리 요청");
-        class Task implements Runnable {
-            Product result;
-            public Task(Product result) {
-                this.result = result;
-            }
+		ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
-            @Override
-            public void run() {
-                int sum = 0;
-                for (int i = 0; i < 10; i++) {
-                    sum += i;
-                }
-//                result.addValue(sum);
-                result.setTotal(sum);
-            }
-        }
-        
-        // 공유객체(외부객체)
-        Product result = new Product();
-        // 두개의 작업을 정의
-        Runnable task1 = new Task(result);
-        Runnable task2 = new Task(result);
+		System.out.println("[작업 처리 요청");
 
-        Future<Product> future1 = executorService.submit(task1, result);
-        Future<Product> future2 = executorService.submit(task2, result);
+		// 공유객체(외부객체)
+		Product result = new Product();
+		// 두개의 작업을 정의
+		for (int i = 0; i < query.length; i++) {
+			Runnable task1 = new ParserThread(query[i], result);
+			Future<Product> future = executorService.submit(task1, result);
+			result = future.get();
+		}
+		System.out.println("[처리 결과]" + result.getTotal());
 
-        try {
-            result = future1.get();
-            result = future2.get();
-            System.out.println("[처리 결과]" + result.getTotal());
-            System.out.println("[작업 처리 완료");
-        }
-        catch (InterruptedException e) {}
-        catch (ExecutionException e) {}
-    }
+//        try {
+////            result = future1.get();
+////            result = future2.get();
+//            System.out.println("[작업 처리 완료");
+//        }
+//        catch (InterruptedException e) {}
+//        catch (ExecutionException e) {}
+	}
 }
