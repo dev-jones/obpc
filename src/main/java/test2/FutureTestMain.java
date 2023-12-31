fd0eb2e21f0eedb02ef9137e5e9609c6f72a6e11
dev-jones <dev-jones@github.com>
2023-12-31 15:35:08 +0900

    Future 예제

--- src/main/java/test2/FutureTestMain.java
+++ src/main/java/test2/FutureTestMain.java
@@ -11,8 +11,13 @@ import com.devjones.obpc.domain.Product;
 public class FutureTestMain {
 
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		String[] query = { "갤럭시 GALAX 지포스 RTX 4060 Ti OC D6 8GB", "AMD 라이젠5-5세대 7500F (라파엘) (멀티팩(정품))",
-				"ASRock B650M-HDV/M.2 대원씨티에스", "삼성전자 DDR5-5600 (16GB) PC5-44800" };
+		long beforeTime = System.currentTimeMillis(); //코드 실행 전에 시간 받아오기
+		String[] query = {
+			"갤럭시 GALAX 지포스 RTX 4060 Ti OC D6 8GB",
+			"AMD 라이젠5-5세대 7500F (라파엘) (멀티팩(정품))",
+			"ASRock B650M-HDV/M.2 대원씨티에스",
+			"삼성전자 DDR5-5600 (16GB) PC5-44800"
+		};
 
 		ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
@@ -26,6 +31,18 @@ public class FutureTestMain {
 			Future<Product> future = executorService.submit(task1, result);
 			result = future.get();
 		}
+//		Runnable task1 = new ParserThread(query[0], result);
+//		Future<Product> future1 = executorService.submit(task1, result);
+//		result = future1.get();
+//		Runnable task2 = new ParserThread(query[1], result);
+//		Future<Product> future2 = executorService.submit(task2, result);
+//		result = future2.get();
+//		Runnable task3 = new ParserThread(query[2], result);
+//		Future<Product> future3 = executorService.submit(task3, result);
+//		result = future3.get();
+//		Runnable task4 = new ParserThread(query[3], result);
+//		Future<Product> future4 = executorService.submit(task4, result);
+//		result = future4.get();
 		System.out.println("[처리 결과]" + result.getTotal());
 
 //        try {
@@ -35,5 +52,9 @@ public class FutureTestMain {
 //        }
 //        catch (InterruptedException e) {}
 //        catch (ExecutionException e) {}
+		
+		long afterTime = System.currentTimeMillis(); // 코드 실행 후에 시간 받아오기
+		long secDiffTime = (afterTime - beforeTime); //두 시간에 차 계산
+		System.out.println("시간차이(m) : "+secDiffTime);
 	}
 }
